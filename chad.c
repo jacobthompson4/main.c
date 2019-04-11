b8f1cff5e9405a8ccf1a6a6963c872d5bd5fc7b2
Jacob Thompson <jacob.thompson@uon.edu.au>
2019-04-11 07:43:12 +0000

    added rotation function

--- chad.c
+++ chad.c
@@ -2,8 +2,12 @@
 
 int main()
 {
-    printf("Hello World");
-    printf("hi");
-    printf("world");
+   
 return 0;
+}
+
+char rotationCypherEncrypt (char key, char term) {
+    term = term -65;
+    if()
+    
 }
